ccb02bd9200505562de213acdd5ca5c5e658e413
lionel-git <lionel.desorme@free.fr>
2019-11-13 14:01:14 +0100

    Add controller

---
+++ TestSwagger/Controllers/MyDataController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestSwagger.Controllers
+{
+    [ApiController]
+    [Route("/MyData/{id}/{name}")]
+    public class MyDataController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<MyData> Get(int id, string name)
+        {
+            var myDatas = new List<MyData>();
+            myDatas.Add(new MyData() { FirstName = "Jean", LastName = "Desorme", Age = 77, Id=$"{id}_{name}" });
+            return myDatas;
+        }
+    }
+}
+
