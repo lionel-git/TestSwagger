f565452a65a8f77de6b94041f4285a877fd69da3
lionel-git <lionel.desorme@free.fr>
2019-11-30 20:58:26 +0100

    Listen on 0.0.0.0

--- TestSwagger/Program.cs
+++ TestSwagger/Program.cs
@@ -20,7 +20,7 @@ namespace TestSwagger
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
-                    webBuilder.UseStartup<Startup>();
+                    webBuilder.UseStartup<Startup>().UseUrls("http://0.0.0.0:5000", "https://0.0.0.0:5001");
                 });
     }
 }
